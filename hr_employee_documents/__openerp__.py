0a8e0b7e75e2393f6f1cc7470c547f658dbec24a
onaoura <osama.naoura@gmail.com>
2015-12-06 21:38:26 +0200

    4

---
+++ hr_employee_documents/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Employee Documents',
+    'version': '1.0',
+    'category': 'Human Resources',
+    'author': 'Osama Naoura',
+    'description': """
+By this module, you can sa.
+    """,
+    'website': '',
+    'depends': ['hr'],
+    'data': [
+        'views/hr_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
